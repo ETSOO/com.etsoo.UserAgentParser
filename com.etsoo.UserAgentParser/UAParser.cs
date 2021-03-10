d0a19933a2dcbe30814cce3f65a0a0a043fdcbf6
Garry Xiao <xz@etsoo.com>
2021-03-10 21:55:47 +1300

    Add project files.

---
+++ com.etsoo.UserAgentParser/UAParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace com.etsoo.UserAgentParser
+{
+    /// <summary>
+    /// User-Agent parser
+    /// 用户代理信息解析
+    /// </summary>
+    public class UAParser
+    {
+    }
+}
